ed300e2e57c69df5b7e31e95fd445c7d351ecf1d
Code045 <maurice@devtigspace.nl>
2025-06-27 03:08:06 +0200

    Modified by Code045

--- woo-ai-category-matcher.php
+++ woo-ai-category-matcher.php
@@ -28,7 +28,7 @@ class Category_Matcher {
             'Woo AI Category Matcher',
             'Woo AI Category Matcher',
             'manage_options',
-            '-matcher',
+            'category-matcher',
             [$this, 'render_admin_page'] // Fixed callback method name
         );
     }
